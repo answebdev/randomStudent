d2c63658e6b02ceed03f6d3541577c520842230b
ANS <36783010+answebdev@users.noreply.github.com>
2020-04-10 18:50:12 -0700

    created User model

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User')
+
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
