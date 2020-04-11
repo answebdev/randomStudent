f6c824e0c56fa22f2a10e4f025c1ac916e965bae
ANS <36783010+answebdev@users.noreply.github.com>
2020-04-10 19:31:42 -0700

    added validation to Register Route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-const User = require('../models/User')
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
