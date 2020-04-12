8b40776abeb7a649c50458e9d0e2c0b8d8652c4d
ANS <36783010+answebdev@users.noreply.github.com>
2020-04-11 19:13:53 -0700

    created the Student Model

---
+++ models/Student.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('student', StudentSchema);
