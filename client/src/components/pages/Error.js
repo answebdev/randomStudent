e0d13b6815d9f8b44a06d638cde13ac3dd50f052
ANS <36783010+answebdev@users.noreply.github.com>
2020-04-12 18:04:36 -0700

    updated Error Page

--- client/src/components/pages/Error.js
+++ client/src/components/pages/Error.js
@@ -3,12 +3,30 @@ import React from 'react';
 const Error = () => {
   return (
     <div>
-      <h1>404 Page Not Found</h1>
-      <h1>
-        <span role='img' aria-label='Face With Rolling Eyes Emoji'>
-          🙄
+      <h1 className='text-center'>404 Page Not Found</h1>
+      <h1 className='text-center'>
+        <span>
+          <i class='far fa-surprise fa-spin'></i>
         </span>
       </h1>
+      <br />
+      <p className='text-center'>
+        <a
+          class='btn btn-primary error-btn'
+          href='/'
+          style={
+            {
+              // fontFamily: 'Roboto',
+              // fontWeight: '400',
+              // letterSpacing: '0.5',
+              // backgroundColor: '#3D6CAD',
+              // border: '#ffffff',
+            }
+          }
+        >
+          <i className='fa fa-home'></i> Take Me Home
+        </a>
+      </p>
     </div>
   );
 };
