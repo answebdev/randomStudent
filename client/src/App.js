a6d48f8c3bebd8e72369c140be7240816a70c808
ANS <36783010+answebdev@users.noreply.github.com>
2020-04-12 17:43:04 -0700

    added React Router, Home, About, and Error pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Error from './components/pages/Error';
 import './App.css';
 
 const App = () => {
   return (
-    <div className='App'>
-      <Navbar />
-      Random Student Generator
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        This is a full-stack React app used to randomly generate student names
+        from a class roster.
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version 1.0.0</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
