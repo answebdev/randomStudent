e0d0d2ac07a08a7d56eb14f239ea4e208bfcae14
ANS <36783010+answebdev@users.noreply.github.com>
2020-04-20 20:03:57 -0700

    added links to Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => {
   return (
@@ -7,6 +8,23 @@ const Navbar = ({ title, icon }) => {
       <p className='nav-title'>
         <i className={icon} /> {title}
       </p>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/generator'>Generator</Link>
+        </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
