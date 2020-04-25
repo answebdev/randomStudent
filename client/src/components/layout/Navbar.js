b118264d74482e8a30dc1c7d9f8814294b120210
ANS <36783010+answebdev@users.noreply.github.com>
2020-04-25 15:48:16 -0700

    updated Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
 const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+
+  const onLogout = () => {
+    logout();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt'></i>
+          <span className='hide-sm'> Logout</span>
+        </a>
+      </li>
+      <li>
+        <Link to='/generator'>Generator</Link>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+      {/* <li>
+        <Link to='/'>Home</Link>
+      </li> */}
+    </Fragment>
+  );
+
   return (
     <div className='navbar bg-primary'>
       <p className='nav-title'>
         <i className={icon} /> {title}
       </p>
       <ul>
-        <li>
+        {isAuthenticated ? authLinks : guestLinks}
+        {/* <li>
+          <Link to='/generator'>Generator</Link>
+        </li> */}
+        {/* <li>
           <Link to='/'>Home</Link>
         </li>
         <li>
           <Link to='/generator'>Generator</Link>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           <Link to='/register'>Register</Link>
         </li>
         <li>
           <Link to='/login'>Login</Link>
-        </li>
+        </li> */}
         <li>
           <Link to='/about'>About</Link>
         </li>
